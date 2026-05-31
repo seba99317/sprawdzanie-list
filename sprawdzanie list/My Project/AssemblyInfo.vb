f88c7f1d098a51fb42872872b5ca61571951ce87
seba99317 <seba99317@github.com>
2026-05-31 15:12:37 +0200

    Add exe v1.1 for auto-update from old versions

---
+++ sprawdzanie list/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+' zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' powiązane z zestawem.
+
+' Sprawdź wartości atrybutów zestawu
+
+<Assembly: AssemblyTitle("sprawdzanie list")>
+<Assembly: AssemblyDescription("sprawdzanie list dla yoradio")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("sprawdzanie list")>
+<Assembly: AssemblyCopyright("Copyright © by seba99317 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Następujący identyfikator GUID jest identyfikatorem elementu typelib w przypadku udostępnienia tego projektu w modelu COM
+<Assembly: Guid("3df46e49-c303-4d18-8300-8ff9dc8608a7")>
+
+' Informacje o wersji zestawu zawierają następujące cztery wartości:
+'
+'      Wersja główna
+'      Wersja pomocnicza
+'      Numer kompilacji
+'      Poprawka
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
